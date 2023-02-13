a1bbea6a93161ce92a8bfc3936a4ce07693035b2
Marko Melle <markmelle1@gmail.com>
2023-02-14 00:12:20 +0100

    modified:   tests/e2e.test.js

--- tests/e2e.test.js
+++ tests/e2e.test.js
@@ -1,23 +1,23 @@
 
 
-// describe('Popover', () => {
-//   // let server = null;
-//   const baseUrl = 'http://localhost:9999';
+describe('Popover', () => {
+  // let server = null;
+  const baseUrl = 'http://localhost:9999';
 
-//   beforeEach(async () => {
-//     await page.goto(baseUrl);
-// });
+  beforeEach(async () => {
+    await page.goto(baseUrl);
+});
 
 
-//   test('should popover toggle', async () => {
-//     await page.goto(baseUrl);
+  test('should popover toggle', async () => {
+    await page.goto(baseUrl);
 
-//     const btn = await page.$('.btn');
+    const btn = await page.$('.btn');
 
-//     await btn.click();
+    await btn.click();
 
-//     await page.waitForSelector('.popover');
-//     await btn.click();
-//     await page.waitForSelector('.popover', { hidden: true });
-//   });
-// });
+    await page.waitForSelector('.popover');
+    await btn.click();
+    await page.waitForSelector('.popover', { hidden: true });
+  });
+});
